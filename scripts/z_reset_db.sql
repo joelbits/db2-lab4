abcad3c2526ac74459306ac4be738f4aa92fc5b6
Joel Bits <joak.dev@gmail.com>
2018-02-09 23:33:40 +0100

    Fixes for new lab tasks from old repo.

--- scripts/z_reset_db.sql
+++ scripts/z_reset_db.sql
@@ -2,5 +2,7 @@ DROP DATABASE IF EXISTS `lab4` ;
 CREATE DATABASE IF NOT EXISTS `lab4`;
 USE `lab4`;
 
-SOURCE /etc/mysql/users.sql;
-SOURCE /etc/mysql/orders.sql;
+SOURCE /scripts/aa_employees.sql;
+SOURCE /scripts/bb_projects.sql;
+SOURCE /scripts/cc_project_members.sql;
+SOURCE /scripts/dd_departments.sql;
