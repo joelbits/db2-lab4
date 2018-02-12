42b49d7d82fdd924fc0aafdd9899c5bd5baf84d1
Joel Bits <joak.dev@gmail.com>
2018-02-12 01:44:06 +0100

    Stuck

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -2,10 +2,18 @@
 
 USE lab4;
 
--- Lab 4 - 1 Foreign keys
+-- Lab 4 - 1 - Foreign keys
 -- Skriv queries (ALTER TABLE) för koppla ihop tabellerna med foreign_keys, ta med lämpliga val för vad som ska hända vid updates och deletes på PK:
 -- departments-mangager till employees-id,
 -- project-supervisor till employees-id
 -- projectmembers-e_id till employees-id
 -- projectmembers-p_id till projects-id
 
+
+-- Lab 4 - 2 - Projects
+-- Skriv queries så att projects ändras (ALTER TABLE) så att varje project alltid har en supervisor, så att två projektnamn inte kan vara samma, och så att det alltid måste finnas ett projektnamn.
+ALTER TABLE projects MODIFY CONSTRAINT FK_Employee_Id
+
+--ALTER TABLE `projects` DROP FOREIGN KEY `FK_Employee_Id`;
+--ALTER TABLE `projects` ADD CONSTRAINT `FK_Employee_Id`
+--    FOREIGN KEY (`FK_Employee_Id`) REFERENCES `employees`.`id` ON DELETE CASCADE ON UPDATE CASCADE;
