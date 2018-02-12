0ec8b8c5ed054b8578b349046a6fe446034a4464
Joel Bits <joak.dev@gmail.com>
2018-02-12 15:01:05 +0100

    WIP

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -2,10 +2,18 @@
 
 USE lab4;
 
--- Lab 4 - 1 Foreign keys
--- Skriv queries (ALTER TABLE) för koppla ihop tabellerna med foreign_keys, ta med lämpliga val för vad som ska hända vid updates och deletes på PK:
--- departments-mangager till employees-id,
--- project-supervisor till employees-id
--- projectmembers-e_id till employees-id
--- projectmembers-p_id till projects-id
+-- Lab 4 - 5 - Skapa vy: salary_data_dept
+-- Skapa en vy som ger avdelningsnamn, minsta lön, hösta lön, medellön och antal anställda för varje avdelning.
+-- ALTER TABLE departments ADD name VARCHAR(30) UNIQUE; -- FROM Lab 4 - 3
 
+DROP VIEW IF EXISTS salary_data_dept;
+CREATE VIEW salary_data_dept AS
+SELECT d.name AS department_name,
+    MIN(e.salary) AS min_salary,
+    MAX(e.salary) AS max_salary,
+    AVG(e.salary) AS avg_salary,
+    (SELECT COUNT(*) FROM employees) AS dep_employees
+FROM departments d, employees e;
+
+-- SELECT * FROM salary_data_dept
+-- GROUP BY department_name;
