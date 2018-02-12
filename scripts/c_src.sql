84cf996e80f509996438eb9b6e8882405af5a2a8
Joel Bits <joak.dev@gmail.com>
2018-02-12 13:42:14 +0100

    2 of 3 done

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -2,10 +2,15 @@
 
 USE lab4;
 
--- Lab 4 - 1 Foreign keys
--- Skriv queries (ALTER TABLE) för koppla ihop tabellerna med foreign_keys, ta med lämpliga val för vad som ska hända vid updates och deletes på PK:
--- departments-mangager till employees-id,
--- project-supervisor till employees-id
--- projectmembers-e_id till employees-id
--- projectmembers-p_id till projects-id
+-- Lab 4 - 5 - Optimera datatyper
+-- Skriv en query som ändrar (ALTER TABLE)  ett par av datatyperna till något som är effektivare. 
+-- a) projects - name behöver inte vara VARCHAR(50). Ändra till något som är mer lagom.
+-- b) departments - id kommer aldrig vara större heltal än 50. Byt till mer lagom datatyp än INT.
+-- c) employees - title - kan bara vara något av "dr", "mr", "ms", "mrs", "rev" eller "honorable". Byt datatyp till ENUM.
+
+-- 4 - 5 - a)
+ALTER table projects MODIFY name VARCHAR(25); -- PROCEDURE ANALYSE() says avg 9 and max 16 with current data. Changed from 50.
+
+-- 4 -5 - b)
+ALTER table departments MODIFY id TINYINT; -- PROCEDURE ANALYSE() says max 12 with current data. Changed from int to tinyint.
 
