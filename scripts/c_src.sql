8a4b55251cf547567ce873594d8c2a3ad60294df
Joel Bits <joak.dev@gmail.com>
2018-02-12 13:14:10 +0100

    Done but needs testing

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -4,3 +4,5 @@ USE lab4;
 
 -- Lab 4 - 4 - Employees
 -- Vi (eller kund) vet att det kommer göras många sökningar och sorteringar på efternamn. Lägg därför in INDEX där. Användarnamn måste också spärras så inte två kan få samma.
+ALTER TABLE employees ADD INDEX (last_name); -- test
+ALTER TABLE employees ADD UNIQUE(login); -- test
