e8b763354eddc90409fa05b34b03700a90d1b551
Joel Bits <joak.dev@gmail.com>
2018-02-12 02:47:51 +0100

    One req working.

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -9,3 +9,6 @@ USE lab4;
 -- projectmembers-e_id till employees-id
 -- projectmembers-p_id till projects-id
 
+-- Lab 4 - 3 - Departments
+-- Ändra i DB så att alla anställda alltid är knutna till en avdelning. Ändra så att default för nya anställda och för anställda utan dept ska vara "Training". Det ska inte kunna finnas anställda utan avdelningar. Lägg också in att deparments måste ha ett namn och det måste vara unikt samt att varje department måste ha en manager.
+ALTER TABLE employees MODIFY department INT NOT NULL; -- WORKS
