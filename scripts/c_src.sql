96910d5508aa1fc1b754e402f8aba2f37cf2c7a4
Joel Bits <joak.dev@gmail.com>
2018-02-12 03:41:28 +0100

    Starting out

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -2,10 +2,5 @@
 
 USE lab4;
 
--- Lab 4 - 1 Foreign keys
--- Skriv queries (ALTER TABLE) för koppla ihop tabellerna med foreign_keys, ta med lämpliga val för vad som ska hända vid updates och deletes på PK:
--- departments-mangager till employees-id,
--- project-supervisor till employees-id
--- projectmembers-e_id till employees-id
--- projectmembers-p_id till projects-id
-
+-- Lab 4 - 4 - Employees
+-- Vi (eller kund) vet att det kommer göras många sökningar och sorteringar på efternamn. Lägg därför in INDEX där. Användarnamn måste också spärras så inte två kan få samma.
