0306f32c4f9a8cd662d897837e4e9bb466f2d96d
joelbits <joak.dev@gmail.com>
2018-02-14 19:29:27 +0700

    Improved comments

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -2,11 +2,11 @@
 -- Lab 4 - 2 - Projects
 -- Skriv queries så att projects ändras (ALTER TABLE) så att varje project alltid har en supervisor (1), så att två projektnamn inte kan vara samma (2), och så att det alltid måste finnas ett projektnamn (3).
 
--- (1)
+-- (1) alter table projects supervisor not null
 ALTER TABLE projects MODIFY supervisor INT NOT NULL;
 
--- (2)
--- Since MySQL ~5.7.4 ALTER IGNORE... does not work anymore. Therefore, first removing duplicates = change projects.name of duplicates to something unique
+-- (2) alter table projecs add constraint unique(name)
+-- Since MySQL ~5.7.4 ALTER IGNORE... does not work. Therefore, first removing duplicates = change projects.name of duplicates to something "unique". Not perfect.
 
  -- Renames existing duplicate names to existing name + some random numbers
 UPDATE projects p
@@ -17,7 +17,7 @@ SET p.name = CONCAT(p.name, ' ABS(CHECKSUM(NewId())) % 10000');
 -- Now since no duplicates should exist for projects.name we can apply UNIQUE(name)
 ALTER TABLE projects ADD CONSTRAINT UNIQUE(name);
 
--- (3)
+-- (3) alter table projects name not null
 ALTER TABLE projects MODIFY name VARCHAR(50) NOT NULL;
 
 -- Usage:
