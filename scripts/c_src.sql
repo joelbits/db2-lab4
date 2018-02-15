53a551b0ab9ed03c339f5483565893486f0eb281
joelbits <joak.dev@gmail.com>
2018-02-15 17:03:44 +0700

    DATETIME logs with time

--- scripts/c_src.sql
+++ scripts/c_src.sql
@@ -1,19 +1,20 @@
--- Author: Joel Åkerblom joak.dev@gmail.com
 
-USE lab4;
-
--- Lab 4 - 8 - Skapa tabell: total_salary
--- Skapa en tabell för summan av löner, lägg in nuvarande värde (gör SELECT SUM(salary)...) och lägg till triggers på anställdas löner så att summan av löner alltid stämmer i nya tabellen. Tabellen ska också ha ett fält för last_update som visar när den senast uppdaterades. Testa och visa med några UPDATE och INSERT att det fungerar.
+/* Lab 4 - 8 - Skapa tabell: total_salary
+Skapa en tabell för summan av löner, lägg in nuvarande värde 
+(gör SELECT SUM(salary)...) och lägg till triggers på anställdas löner
+så att summan av löner alltid stämmer i nya tabellen. Tabellen ska 
+också ha ett fält för last_update som visar när den senast uppdaterades. 
+Testa och visa med några UPDATE och INSERT att det fungerar. */
 
 DROP TABLE IF EXISTS total_salary;
 
 CREATE TABLE total_salary (
     total_sum INT,
-    last_update DATE
+    last_update DATETIME
 );
 
 INSERT INTO total_salary (total_sum, last_update)
-VALUES ((SELECT SUM(salary) FROM employees), DATE(NOW()));
+VALUES ((SELECT SUM(salary) FROM employees), NOW());
 
 SELECT * FROM total_salary;
 
